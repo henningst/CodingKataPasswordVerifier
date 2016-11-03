864808225d93b31d792fce879dbe6eeb70854906
Henning Støverud <henning.stoverud@hr-manager.net>
2016-11-03 13:52:06 +0100

    Implemented null check to satisfy the test for passwords that are null

--- src/PasswordVerifier.cs
+++ src/PasswordVerifier.cs
@@ -6,6 +6,11 @@ namespace ConsoleApplication
     {
         public bool Verify(string password)
         {
+            if(password == null) 
+            {
+                throw new ArgumentException("The password should not be null");
+            }
+
             if(password.Length <= 8) {
                 throw new ArgumentException("The password must be longer than 8 chars");
             }
