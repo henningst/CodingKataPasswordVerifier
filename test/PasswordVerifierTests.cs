ad955f25ea0678f5c6f19f5c802ab7c3eb212aaa
Henning Støverud <henning.stoverud@hr-manager.net>
2016-11-03 13:42:24 +0100

    Added new test to verify that the exception message is useful

--- test/PasswordVerifierTests.cs
+++ test/PasswordVerifierTests.cs
@@ -23,5 +23,12 @@ namespace Tests
         {
             Assert.Throws<ArgumentException>(() => _passwordVerifier.Verify("shortpw"));
         }
+
+        [Fact]
+        public void Verify_WhenPassword8CharsOrShorter_ShouldHaveUsefulExceptionMessage() 
+        {
+            var exception = Assert.Throws<ArgumentException>(() => _passwordVerifier.Verify("shortpw"));
+            Assert.Equal("The password must be longer than 8 chars", exception.Message);
+        }        
     }
 }
