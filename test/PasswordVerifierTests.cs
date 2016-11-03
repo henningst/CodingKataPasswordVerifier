d93b299c2c7b9e4fc78472b6adc2753e150c343e
Henning Støverud <henning.stoverud@hr-manager.net>
2016-11-03 13:31:12 +0100

    Adding happy path test checking password larger than 8 chars

--- test/PasswordVerifierTests.cs
+++ test/PasswordVerifierTests.cs
@@ -6,5 +6,10 @@ namespace Tests
 {
     public class PasswordVerifiertests 
     {
+        [Fact]
+        public void Verify_WhenPasswordLongerThan8Chars_ShouldReturnTrue() {
+            var passwordVerifier = new PasswordVerifier();
+            Assert.True(passwordVerifier.Verify("mypassword"));
+        }        
     }
 }
